b95fb96a80c0494e35eed0b0d0c7a8e24894c2d6
hhrum <denis.smit72@gmail.com>
2021-01-30 11:56:28 +0500

    Чек лист. 4

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import bridge from '@vkontakte/vk-bridge';
 
 Vue.config.productionTip = false
 
+window.bridge = bridge;
+
+window.bridge.send('VKWebAppInit')
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
